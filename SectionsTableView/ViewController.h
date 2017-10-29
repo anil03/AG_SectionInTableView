d7ec267a65b90d4f1a0044706d15d552773023fd
anil gupta <anil03gupta03@gmail.com>
2017-10-29 12:13:25 +0530

    Initial Commit

---
+++ SectionsTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SectionsTableView
+//
+//  Created by Anil Gupta on 29/10/17.
+//  Copyright © 2017 Anil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SectionsTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SectionsTableView
+//
+//  Created by Anil Gupta on 29/10/17.
+//  Copyright © 2017 Anil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
